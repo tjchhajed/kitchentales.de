ae03f0150febe55e77c0d8f6cf65ad1c5b58b537
Surajit Dutta <suraj.roland@gmail.com>
2017-02-18 12:47:25 +0100

    initial commit

--- js/landing-page.js
+++ js/landing-page.js
@@ -29,3 +29,65 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+/* transparency of the navigation bar with scrolling*/
+function checkScroll(){
+    var startY = $('.navbar').height() * 5; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
+/* fadeIn animation with scroll (applicable anywhere in the html body)*/
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.fadeIn').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+            }
+        });
+    });
+});
+
+/* testimonial slider */
+$(document).ready(function() {
+$('.owl-carousel').owlCarousel({
+    items:3,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:false,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    margin:30,
+    stagePadding:30,
+  });
+});
+
+/* scroll based parallax */
+$(document).ready(function() {
+    /* css + js driven API */
+    AOS.init();
+    /* pure js driven API */
+    skrollr.init();
+});
+
+/* blur background image on scroll */
+$(window).on('scroll', function () {
+    var pixs = $(window).scrollTop();
+    opacityVal = (pixs / 150.0);
+    $(".bg-img-blur").css('opacity',opacityVal);
+});
