aae9170ec2e2dfc13168285b027f7409c2a1d9cf
Surajit Dutta <suraj.roland@gmail.com>
2017-03-17 23:08:23 +0100

    delta commit

--- js/landing-page.js
+++ js/landing-page.js
@@ -100,3 +100,32 @@ $(document).ready(function() {
 		})(jQuery);
 
 document.getElementById("audio").volume = 0.5;
+
+
+var position = [27.1959739, 78.02423269999997];
+
+function showGoogleMaps() {
+
+    var latLng = new google.maps.LatLng(position[0], position[1]);
+
+    var mapOptions = {
+        zoom: 16, // initialize zoom level - the max value is 21
+        streetViewControl: false, // hide the yellow Street View pegman
+        scaleControl: true, // allow users to zoom the Google Map
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: latLng
+    };
+
+    map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
+
+    // Show the default red marker at the location
+    marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', showGoogleMaps);
